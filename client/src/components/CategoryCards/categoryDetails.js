3527e84bc06c87b9fa3c3e5d7e7036c2b70b630f
Beanalini <dat826@gmail.com>
2022-12-05 17:56:13 +0000

    Modified home page

---
+++ client/src/components/CategoryCards/categoryDetails.js
@@ -0,0 +1,22 @@
+export const categoryDetails = [
+    
+    {
+        id: 1 ,
+        title: "testing1"
+    },
+    
+    {
+        id: 2 ,
+        title: "testing2"
+    },
+    {
+        id: 3 ,
+        title: "testing3"
+    },
+    {
+        id: 4 ,
+        title: "testing4"
+    },
+    
+    
+]
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,44 +3,39 @@ import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
 import bms_logo from '../../assets/images/stuff.png'
 import './index.css';
-//import { Nav } from 'react-bootstrap';
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function Nav() {
+function Navtest() {
 
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (               
         <>
-          <li className="nav-item fs-3">
-            <Link to="/orderHistory">
-              Order History
-            </Link>
-          </li>
-          <li className="nav-item fs-3">
+        <Link className="nav-link" to="/orderHistory">Home</Link>
+          
             {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-            <a href="/" onClick={() => Auth.logout()}>
-              Logout
-            </a>
-          </li>
+            <Nav.Link href="/" onClick={() => Auth.logout()}>Logout</Nav.Link>
+            
+           
        </>
       );
     } else {
       return (       
        <>
-          <li className="nav-item fs-3">
+          
             <Link to="/signup" className="nav-link">
               Signup
             </Link>
-          </li> {''}
-          <li className="nav-item fs-3">
+          
             <Link to="/login" className="nav-link">
               Login
             </Link> 
-          </li>
+          
         </>
       );
     }
@@ -48,32 +43,40 @@ function Nav() {
   }
 
   return (
-    <>
-    <header className="navbar nav custom-header">
-    <div className="container-fluid nav">
-      <ul className='nav justify-content-end'>
-        <Link to="/">
-        <img className="navbar-brand m-2" src={bms_logo} alt="logo" class="logo"/>          
-          
+    
+    <header className="custom-header">             
+        <Navbar bg="dark" expand="lg" variant="dark">
+      <Container>
+      <Link to="/donate">
+        <img className="navbar-brand" src={bms_logo} alt="logo" class="logo"/>            
         </Link>
-            <li className="nav-item fs-3">
-         <Link to="/donate" className="nav-link">Donate</Link>
-  </li>
-
-   {showNavigation()}
-
-      <nav className="navbar">
-         
-      </nav>
-      </ul>
-      </div>
-    
-   </header>
-
-  </>
-
-    
+       
+        
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Link className="nav-link" to="/">Home</Link>
+            <Nav.Link href="#link">Your Listed Items</Nav.Link>
+            {showNavigation()}
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="/">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/Your listed Items">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/order-history">Order History</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/donate">
+                Support Us
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+     </header>
   );
 }
 
-export default Nav;
+export default Navtest;
