e8189096a4f4c1c761963a25b2306052fe99b87f
nxtera <64105557+nxtera@users.noreply.github.com>
2022-12-05 19:00:37 +0000

    Added sample images. Added Carousel to main page. Imported query function and passed it into carousel props.
    On branch carousel
    Changes to be committed:
    	modified:   client/.eslintcache
    	new file:   client/src/assets/images/user images of stuff/Cobra Petrol Lawnmower RM514SPC.webp
    	new file:   client/src/assets/images/user images of stuff/VIKING MB248T - SELF PROPELLED LAWNMOWER.JPG
    	new file:   client/src/assets/images/user images of stuff/camera.jpg
    	new file:   client/src/components/Carousel/flickity.css
    	new file:   client/src/components/Carousel/index.js
    	new file:   client/src/components/Carousel/style.css
    	modified:   client/src/pages/Home.js
    	modified:   package-lock.json
    	modified:   package.json

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { Button } from 'react-bootstrap' 
 import Cart from "../components/Cart";
+import Carousel from "../components/Carousel";
+import { useQuery } from '@apollo/client';
+import { QUERY_ALL_PRODUCTS } from "../utils/queries"
 
 const Home = () => {
+  const { loading, data } = useQuery(QUERY_ALL_PRODUCTS);
+  const products = data?.products || [];
+
+const topProduct = [];
+
+// topProduct.push(products[i])
+
   return (
     <div className="container">
       <h1>This is the Home page</h1>
       <Button type="Button" class="btn btn-Primary">Primary</Button>
-  
+      <Carousel products ={products} />
       <Cart />
     </div>
   );
