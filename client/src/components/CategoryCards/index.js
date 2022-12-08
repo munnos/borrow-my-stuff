15776222de75f42ee3c65ad4de3ac1b9f5e49bed
nxtera <64105557+nxtera@users.noreply.github.com>
2022-12-08 14:17:14 +0000

    Changed garden equipment to equipment CategoryCards maps through Category query and displays CategoryCards Added style to CategoryCards
    On branch mainpageCategories
    Changes to be committed:
    	modified:   client/.eslintcache
    	new file:   client/src/assets/css/app.css
    	modified:   client/src/components/CategoryCards/index.js
    	new file:   client/src/components/CategoryCards/style.css
    	modified:   client/src/index.js
    	modified:   client/src/pages/Home.js
    	modified:   client/src/utils/queries.js
    	modified:   server/seeders/listingCategoryData.json

--- client/src/components/CategoryCards/index.js
+++ client/src/components/CategoryCards/index.js
@@ -2,31 +2,38 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import "./style.css"
 
 import React from "react";
 
 
-const CategoryCards = () => {
-    
+const CategoryCards = (props) => {
+  let categories = props.categories;
+  console.log(props.categories)
+  // if (!categories.length) {
+  //   return <h3>No Categories Yet</h3>;
+  // }
     return (
-      
-      
-
-      <Row>
-        <Col>
-        <Card style={{ width: '15rem' }}>
-          <Card.Img variant="top" src="https://picsum.photos/320/200/" />
+      <Container fluid>
+      <Row className="justify-content-md-center">
+        {categories && 
+          categories.map((category) => (
+        <Col className="">
+        <a id="categoryLink" href ="">
+        <Card id="categoryCard" style={{ width: '13rem'}} key={category._id}>
+          <Card.Img id="categoryImg" variant="top" src={category.image} />
           <Card.Body>
-            <Card.Title>Garden Tools</Card.Title>
-            
-            <Button variant="primary">Go somewhere</Button>
+            <Card.Title>{category.name}</Card.Title>
+            {/* <Button variant="primary">Go somewhere</Button> */}
           </Card.Body>
       </Card>
+      </a>
       </Col>
+      ))}
       </Row>
-
-    
-    )
+      </Container>
+    );
   };
   
   export default CategoryCards;
