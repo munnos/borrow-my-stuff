2ac3dc52e4deb2a8796c2da337cf891f60401975
Amal Hirsi <ahirsi@hotmail.co.uk>
2022-12-10 19:22:00 +0000

    ListedProduct page 1

---
+++ client/src/pages/ListedProduct.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Card from 'react-bootstrap/Card';
+import { Button } from 'react-bootstrap';
+import { useQuery } from '@apollo/client';
+import { useParams } from 'react-router-dom';
+import { LISTED_PRODUCTS } from '../utils/queries';
+
+const ListedProduct = () => {
+    // queries are run here
+    // 1. get an id to run a query to get an item ?
+    const {_id} = useParams(); // this comes from another component
+
+    const { loading, data } = useQuery(LISTED_PRODUCTS, {
+      // pass URL parameter
+      variables: { _id: _id },
+    });
+  
+    const item = data?.ListedProduct || {};
+  
+    // 2. use useQuery to run the query to fetch a product by id
+    // store the data in a const called 'item'
+
+
+
+    
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+        return (
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={item.image} />
+            <Card.Body>
+              <Card.Title>{item.name}</Card.Title>
+              <Card.Text>
+                S{item.description}
+              </Card.Text>
+              <Button variant="primary">Request Item</Button>
+            
+            </Card.Body>
+          </Card>
+        );
+      }
+    export default  ListedProduct ;
+    
