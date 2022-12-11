47e08693e5ebc8d5e3ff872c8b4f532d08804f4c
nxtera <64105557+nxtera@users.noreply.github.com>
2022-12-11 13:55:43 +0000

    Added query listed page.

--- client/src/components/Listed.js
+++ client/src/components/Listed.js
@@ -1,11 +1,38 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+import { GET_MY_LISTED_ITEMS } from "../utils/queries";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 
 function Listed( { profileView }) {
     console.log("inside request component");
+    const { loading, data } = useQuery(GET_MY_LISTED_ITEMS);
+    const listedProducts = data?.getListedProductsByUser || [];
+    console.log(data)
+
+
     
   return (
     <h1>From the listed component { profileView }</h1>
-  );
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    );
 }
 
 export default Listed;
