dc5923c763caaa3507d09ef14af4749f74ab542b
Beanalini <dat826@gmail.com>
2022-12-11 22:01:27 +0000

    update to approval status

--- client/src/components/ItemRequestList/index.js
+++ client/src/components/ItemRequestList/index.js
@@ -2,12 +2,50 @@ import React from "react";
 import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col, Button, Card }from 'react-bootstrap';
+import { useMutation } from "@apollo/client";
+import { CHANGE_REQUEST_STATUS } from  '../../utils/mutations'; 
 
-
-function Listed({ requests }) {
+function Listed({ requests}) {
 
     console.log("inside request component");
     console.log(requests);
+    console.log(requests)
+    const [ EditRequestedProduct, { error } ] = useMutation(CHANGE_REQUEST_STATUS)
+
+    const handleApprove = async (e) => {
+      e.preventDefault();
+      console.log(e.target.value);
+
+      
+
+      // try {
+      //   const { data } = await EditRequestedProduct({
+      //     variables: {
+      //       listingRequest,
+      //       listingProduct,
+      //       decision,
+                               
+      //     },
+      //   });
+        
+  
+      //  } catch (err) {
+      //   console.error(err);
+
+      //  }
+
+      
+
+    }
+
+    const handleReject = async (e) => {
+      e.preventDefault();
+      console.log(e.target.value);
+      
+
+
+
+    }
     
   return (
     
@@ -21,18 +59,16 @@ function Listed({ requests }) {
         {requests && 
           requests.map((request) =>  (
                       
-            <Card className="mt-2">
-                <Card.Header>{request.dateRequested}</Card.Header>
+            <Card className="mt-2" key={request._id}>
+                <Card.Header>Requested by: {request.requestee.firstName}</Card.Header>
+                <Card.Header>Date requested: {request.dateRequested}</Card.Header>
+                <Card.Header>Date requested: {request.requestee.email}</Card.Header>
+                <Card.Header>Approval Status: {}</Card.Header>               
                 <Card.Body>        
-                    <Card.Text>
-                            {request.requestee.firstName}
-                    </Card.Text>
+                <Button variant="outline-success" value="approve" onClick={handleApprove} type="submit">Approve</Button>{' '}
+                {/* <Button variant="outline-danger"value="reject"   onClick={event => handleReject(event, key)} key={request._id} type="submit">Reject</Button> */}
                 </Card.Body>
-                <Card.Body>        
-                    <Card.Text>
-                            {request.description}
-                    </Card.Text>
-                </Card.Body>            
+                         
             </Card>
             ))}
       
