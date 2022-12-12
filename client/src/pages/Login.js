b178e334267c8adc7066a1f3036de6153098b520
Beanalini <dat826@gmail.com>
2022-12-12 16:12:10 +0000

    Login and sign up form make over with React ;-)

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,6 +3,7 @@ import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
+import { Button, Col, Container, Row, Form, Card} from "react-bootstrap";
 
 function Login(props) {
   const [formState, setFormState] = useState({ email: '', password: '' });
@@ -30,40 +31,50 @@ function Login(props) {
   };
 
   return (
-    <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
+   
+    <Container fluid>
+    <Row className = "mt-4">
+    <Col md={{ span: 8, offset: 2 }}>
+    <Link to="/signup">← Go to Signup</Link>
+    <Card className="text-center bg-secondary text-white my-5 py-4">
+           <h3>Login </h3>                
+       </Card>
+    <Card className='mt-3' border="primary"> 
+     
+      <form border = "success" onSubmit={handleFormSubmit}>
+      <Form.Group className="mt-3" controlId="formBasicEmail">
+        <Form.Label htmlFor="email">Email address:</Form.Label>
+        <Form.Control type="email" 
+            placeholder="Enter email"
+            name="email"           
             id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
+            onChange={handleChange} />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label htmlFor="pwd">Password:</Form.Label>
+        <Form.Control
+         type="password"
+         placeholder="Password"
+         name="password"
+         id="pwd"
+        onChange={handleChange} />
+      </Form.Group>      
         {error ? (
           <div>
-            <p className="error-text">The provided credentials are incorrect</p>
+          <p className="error-text">The provided credentials are incorrect</p>     
           </div>
         ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
+        
+        <Button className="mt-3 mb-3"variant="primary" type="submit">Submit </Button> 
+       
       </form>
-    </div>
+      </Card>
+      </Col>
+      </Row>
+    </Container>
   );
 }
 
