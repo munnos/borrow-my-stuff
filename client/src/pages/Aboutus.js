4e5d08011ee4415ac98df83f2981e768ccd2df10
Amal Hirsi <ahirsi@hotmail.co.uk>
2022-12-12 14:00:10 +0000

    Start to about us page

---
+++ client/src/pages/Aboutus.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CategoryMenu from "../components/CategoryMenu";
+import Cart from "../components/Cart";
+
+const Aboutus = () => {
+  return (
+
+    <div className="container">    
+   
+
+</div>
+
+    );
+  };
+    export default Aboutus;
